f61ccc3aa1703e10684b33d84cd90428bf44252b
Saleh12652 <salihaalmalki20@icloud.com>
2025-12-13 17:08:04 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,8 +1,12 @@
+// models/User.js
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    passwordHash: { type: String, required: true }
+  username: { type: String, required: true, unique: true },
+  passwordHash: { type: String, required: true },
+  role: { type: String, enum: ['admin', 'donor', 'staff'], default: 'donor' },
+  name: { type: String },
+  createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('User', UserSchema);
