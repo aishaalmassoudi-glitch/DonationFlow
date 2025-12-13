2dc074b5f5fbbdd93b20de4ca235e5dbdb3528b6
Saleh12652 <salihaalmalki20@icloud.com>
2025-12-13 17:06:58 +0300

    Update Donation.js

--- models/Donation.js
+++ models/Donation.js
@@ -1,3 +1,4 @@
+// models/Donation.js
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -5,6 +6,8 @@ const DonationSchema = new Schema({
   donorId: { type: Schema.Types.ObjectId, ref: 'Donor', required: true },
   caseId: { type: Schema.Types.ObjectId, ref: 'Case', required: true },
   amount: { type: Number, required: true },
+  description: { type: String },
+  userId: { type: Schema.Types.ObjectId, ref: 'User' }, // who recorded it
   date: { type: Date, default: Date.now }
 });
 
